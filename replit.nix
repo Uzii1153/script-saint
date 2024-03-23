d9e93413aa509f2abb4aa23d19632efdf9ff6c64
Uzii1153 <164149858+Uzii1153@users.noreply.github.com>
2024-03-23 16:29:38 +0700

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -3,11 +3,9 @@
 
         pkgs.nodejs-18_x
         #pkgs.libwebp
-	      pkgs.nodePackages.typescript
         pkgs.ffmpeg
         pkgs.imagemagick  
         pkgs.wget
         pkgs.git
-        pkgs.nodePackages.pm
   ];
 }
